799e62f087600260a2f9daf94c524196e29e3a7a
Cisc474 account <cisc474@localhost>
2015-05-17 12:05:19 -0400

    finished

--- api/index.php
+++ api/index.php
@@ -8,48 +8,102 @@ $uri = $_SERVER['REQUEST_URI'];
 
 $url_parts = parse_url($uri);
 $path = $url_parts["path"];
-$parameters = $url_parts["query"];
+//$parameters = $url_parts["query"];
 
 $prefix = "api";
 $ind = strpos($path, $prefix);
 $request = substr($path, $ind + strlen($prefix));
 
-echo "$request\n<br>";
-echo "$verb\n<br>";
-echo json_encode($_REQUEST)."\n<br>";
-
-if (!( isset($_REQUEST["ball_name"])
-    && isset($_REQUEST["ball_date"])
-    && isset($_REQUEST["ball_address"])
-    && isset($_REQUEST["ball_city"])
-    && isset($_REQUEST["ball_state"])
-    && isset($_REQUEST["ball_zip"])
-    && isset($_REQUEST["ball_home_phone"])
-    && isset($_REQUEST["ball_cell_phone"])
-    && isset($_REQUEST["ball_email"])
-    && isset($_REQUEST["ball_rh"])
-    && isset($_REQUEST["ball_lh"])
-    && isset($_REQUEST["ball_conv"])
-    && isset($_REQUEST["ball_id"])
-    && isset($_REQUEST["ball"])
-    && isset($_REQUEST["ball_weight"])
-    && isset($_REQUEST["ball_text"])
-    && isset($_REQUEST["ball_layout"])
-    && isset($_REQUEST["ball_surface"])
-    && isset($_REQUEST["ball_bh"])
-    && isset($_REQUEST["ball_size"])
-    && isset($_REQUEST["ball_depth"])
-    && isset($_REQUEST["ball_pap_h"])
-    && isset($_REQUEST["ball_pap_v"])
-    && isset($_REQUEST["ball_pap_clt"])
-    && isset($_REQUEST["ball_thumb"])
-    && isset($_REQUEST["ball_fingers"])
-    && isset($_REQUEST["ball_price"])
-    && isset($_REQUEST["ball_employee"])
-)) {
-	header('HTTP/1.1 400 Form not completed');
-} else {
-
+//echo "$request\n<br>";
+//echo "$verb\n<br>";
+//echo json_encode($_REQUEST)."\n<br>";
+if ($verb = 'GET') {
+    if ($request == "/new") {
+        if (!( isset($_REQUEST["ball_name"])
+            && isset($_REQUEST["ball_date"])
+            && isset($_REQUEST["ball_address"])
+            && isset($_REQUEST["ball_city"])
+            && isset($_REQUEST["ball_state"])
+            && isset($_REQUEST["ball_zip"])
+            && isset($_REQUEST["ball_home_phone"])
+            && isset($_REQUEST["ball_cell_phone"])
+       	    && isset($_REQUEST["ball_email"])
+            && isset($_REQUEST["ball_rh"])
+            && isset($_REQUEST["ball_lh"])
+            && isset($_REQUEST["ball_conv"])
+            && isset($_REQUEST["ball_id"])
+            && isset($_REQUEST["ball"])
+            && isset($_REQUEST["ball_weight"])
+            && isset($_REQUEST["ball_pin"])
+            && isset($_REQUEST["ball_layout"])
+            && isset($_REQUEST["ball_surface"])
+            && isset($_REQUEST["ball_bh"])
+            && isset($_REQUEST["ball_size"])
+            && isset($_REQUEST["ball_depth"])
+            && isset($_REQUEST["ball_pap_h"])
+            && isset($_REQUEST["ball_pap_v"])
+            && isset($_REQUEST["ball_pap_clt"])
+            && isset($_REQUEST["ball_thumb"])
+            && isset($_REQUEST["ball_fingers"])
+            && isset($_REQUEST["ball_price"])
+            && isset($_REQUEST["ball_employee"])
+        )) {
+            header('HTTP/1.1 400 Form not completed');
+        } else {
+            $dbhandle = new MongoClient();
+            $db = $dbhandle->bowling;
+            $sql = "db.balls.insert({" .
+       	        "'name': '$_REQUEST[ball_name]',".
+                "'date': '$_REQUEST[ball_date]',".
+                "'address': '$_REQUEST[ball_address]',".
+                "'city': '$_REQUEST[ball_city]',".
+                "'state': '$_REQUEST[ball_state]',".
+                "'zip': '$_REQUEST[ball_zip]',".
+                "'home_phone': '$_REQUEST[ball_home_phone]',".
+                "'cell_phone': '$_REQUEST[ball_cell_phone]',".
+                "'email': '$_REQUEST[ball_email]',".
+                "'right_hole': '$_REQUEST[ball_rh]',".
+                "'left_hole': '$_REQUEST[ball_lh]',".
+                "'conv': '$_REQUEST[ball_conv]',".
+                "'id': '$_REQUEST[ball_id]',".
+                "'style': '$_REQUEST[ball]',".
+                "'weight': '$_REQUEST[ball_weight]',".
+                "'text': '$_REQUEST[ball_text]',".
+                "'layout': '$_REQUEST[ball_layout]',".
+                "'surface': '$_REQUEST[ball_surface]',".
+                "'bh': '$_REQUEST[ball_bh]',".
+                "'size': '$_REQUEST[ball_size]',".
+                "'depth': '$_REQUEST[ball_depth]',".
+                "'pap_h': '$_REQUEST[ball_pap_h]',".
+                "'pap_v': '$_REQUEST[ball_pap_v]',".
+                "'pap_clt': '$_REQUEST[ball_pap_clt]',".
+                "'thumb': '$_REQUEST[ball_thumb]',".
+                "'fingers': '$_REQUEST[ball_fingers]',".
+                "'price': '$_REQUEST[ball_price]',".
+                "'employee': '$_REQUEST[ball_employee]',".
+            "})";
+            $sql = "return " . $sql . ";";
+            echo "$sql\n<br>";
+            $output = $db->execute("$sql");
+            //echo print_r($output)."\n<br>";
+            $output_json = json_encode($output);
+            echo "$output_json";
+        }
+    } else if ($request == "/read") {
+        $dbhandle = new MongoClient();
+        $name_clause = [];
+        if (isset($_REQUEST["ball_name"]) && $_REQUEST["ball_name"] != "") $name_clause = ["name" => ['$regex' => "$_REQUEST[ball_name]"]];
+        //echo print_r($name_clause);
+        $db = $dbhandle->bowling;
+        $collection = $db->selectCollection("balls");
+        $cursor = $collection->find($name_clause);
+        echo "[";
+        while ($row = $cursor->getNext()) {
+            echo json_encode($row);
+            if ($cursor->hasNext()) echo ",";
+        }
+        echo "]";
+    }
 }
 /*
 if ($request == "/games") {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 function saveData(){
 
 	var name = document.getElementById('ball_name').value;
+	var address = document.getElementById('ball_address').value;
 	var date = document.getElementById('ball_date').value;
 	var city = document.getElementById('ball_city').value;
 	var state = document.getElementById('ball_state').value;
@@ -28,43 +29,93 @@ function saveData(){
 	var price = document.getElementById('ball_price').value;
 	var completedBy = document.getElementById('ball_employee').value;
 
+	if(name==="" || address==="" || date==="" || city==="" || state==="" ||
+		zip==="" || homePhone==="" || cellPhone==="" || email==="" || leftHand==="" ||
+		rightHand==="" || conv==="" || ft==="" ||  ball==="" || weight==="" || 
+		pin==="" || layout==="" || surface==="" || bhPosition==="" || size==="" ||
+		depth==="" || horizontal==="" || vertical==="" || offset==="" || thumb==="" ||
+		fingers==="" || price==="" || completedBy===""){
+	
+	}
+	else{
 
 	/* Start packing the JSON */
-	var text = '{ "Bowling Data" : [' +
-	'{ "name": ' + name + 
-    	' , "date"' + date +
-    	' , "address"' + address +
-    	' , "city"' + city +
-    	' , "state"' + state +
-    	' , "zip"' + zip +
-    	' , "home_phone"' + homePhone +
-    	' , "cell_phone"' + cellPhone +
-    	' , "email"' + email +
-    	' , "left_hand"' + leftHand +
-    	' , "right_hand"' + rightHand +
-    	' , "conv"' + conv +
-    	' , "ft"' + ft +
-    	' , "ball"' + ball +
-    	' , "weight"' + weight +
-    	' , "pin"' + pin +
-    	' , "layout"' + layout +
-    	' , "surface"' + surface +
-    	' , "bh_postion"' + bhPosition +
-    	' , "size"' + size +
-    	' , "depth"' + depth +
-    	' , "pap_horizontal"' + horizontal +
-    	' , "pap_vertical"' + vertical +
-    	' , "offset"' + offset +
-    	' , "thumb_style"' + thumb +
-    	' , "fingers_style"' + fingers +
-    	' , "price"' + price +
-    	' , "completed_by"' + completedBy +
-    	' }' + ' ]}';
+	var bowlingData =
+		{"ball_name" : name,
+		"ball_date" : date,
+		"ball_address" : address,
+		"ball_city" : city,
+		"ball_state" : state,
+		"ball_zip" : zip,
+		"ball_home_phone" : homePhone,
+		"ball_cell_phone" : cellPhone,
+		"ball_email" : email,
+		"ball_lh" : leftHand,
+		"ball_rh" : rightHand,
+		"ball_conv" : conv,
+		"ball_id" : ft,
+		"ball" : ball,
+		"ball_weight" : weight,
+		"ball_pin" : pin,
+		"ball_layout" : layout,
+		"ball_surface" : surface,
+		"ball_bh" : bhPosition,
+		"ball_size" : size,
+		"ball_depth" : depth,
+    	"ball_pap_h" : horizontal,
+		"ball_pap_v" : vertical,
+		"ball_pap_clt" : offset,
+		"ball_thumb" : thumb,
+		"ball_fingers" : fingers,
+		"ball_price" : price,
+		"ball_employee" : completedBy };
 
-	var bowling_json = JSON.parse(text);
+	jQuery.ajax("api/new", { "data" : bowlingData });
+	location.reload();
+}
 }
 
-function search(firstName, lastName, street, city, state, zipCode, homePhone, cellPhone, email){
+
+function search(){
 //NEED TO IMPLEMENT
+    var name = document.getElementById('ball_name').value;
+    var data = {"ball_name":name};
+    console.log(data);
+	jQuery.ajax({
+
+        url: 'api/read',
+
+        type: "GET",
+
+        data: data,//data,
+
+        success: function (data) {
+            $('.output').html("");
+            if (!data) return;
+            var resultset = JSON.parse(data);
+            if (!resultset[0]) return;
+            console.log(resultset);
+			$('.output').html("<table><tr>");
+            jQuery.each(resultset[0], function(key,val) {
+                if (key != "_id") $('.output').append("<th>" + key + "</th>");
+            });
+            $('.output').append("</tr>");
+            resultset.forEach( function (value,index,array) {
+                $('.output').append("<tr>");
+                jQuery.each(value, function(key,val) {
+                    if (key != "_id") $('.output').append("<td>" + val + "</td>");
+                });
+                $('.output').append("</tr>");
+            })
+            $('.output').append("</table>");
+
+        },
+
+        error: function () {
+            $('body').html("Error happened");
+        }
+
+    });
+	
 
 }
